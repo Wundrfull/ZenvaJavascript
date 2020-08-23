e344c666fabed9e3ba05e673b5cc1ef33afb006e
Noah Peloquin <wundrfull.us@gmail.com>
2020-08-23 01:42:06 -0400

    8-16

---
+++ BasicJavaScript/base.js
@@ -0,0 +1,9 @@
+var i = 5;
+console.log(i);
+
+// ez comments
+
+/**
+ * REALLY EZ COMMENTS
+ * If anyone sees this, I'm sorry, but I love you.
+ */
---
+++ BasicJavaScript/variables.js
@@ -0,0 +1,22 @@
+/**
+ * Will go over data types
+ *
+ * Numbers
+ * Booleans
+ * Strings
+ * Constants
+ */
+
+// Variables are data values that are stored in a memory location.
+
+var age = 27;
+var money = 50.05;
+
+age = 69;
+
+var isUserLoggedIn = true;
+
+var name = 'Bungtown';
+name = 'UpdatedBungtown';
+
+const sucky = 'meh';
