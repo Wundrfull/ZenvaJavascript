110a4fd280a54e82c4e89b168a1bdf48bb720493
Noah Peloquin <wundrfull.us@gmail.com>
2020-10-18 00:00:28 -0400

    Added git ignore

---
+++ Node.js and Express For Beginners/info.js
@@ -0,0 +1,23 @@
+/** ============ INFO FILE ============ **/
+
+// LEARNING GOALS //
+
+/**
+ * - Node.js
+ * - Express.js
+ * - Passport.js
+ * - Mongoose.js
+ * - JSON Web Tokens (JWT)
+ * - MongoDB
+ */
+
+/**
+ * STEPS
+ * 1. User sends request to Express Server (Username, Password)
+ * 2. Validate Username + Password with Database (MongoDB)
+ * 3. Generate JWT (JSON Web Token)
+ * 4. Add JWT to user's cookies
+ * 5. Moving forward, we can use JWT for authentication
+ *
+ * Will be going over other devtools like Nodemon, Postman, and MongoDB compass.
+ */
