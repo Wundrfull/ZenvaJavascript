d40971ee86ce43e313b29a19aed21a2b5dcf6942
Noah Peloquin <wundrfull.us@gmail.com>
2020-10-21 21:08:54 -0400

    Progress on the Node.js Server

--- Node.js and Express For Beginners/info.js
+++ Node.js and Express For Beginners/info.js
@@ -30,3 +30,51 @@
  *
  * - Yopu can use app.all() to load middleware functions at a particular path for all HTTP request methods.
  */
+
+/**
+ * Postman
+ *
+ * - If you sign up of r a Postman account, you can backup your collections
+ *   to your account and use them on multiple computers.
+ *
+ * - In addition to this, you can generate a link to your collection and share that link with others.
+ *
+ * - You can also export your collection as a JSON file and share that file with others.
+ */
+
+/**
+ * Nodemon
+ *
+ * - You can install Nodemon using NPM via: npm install nodemon
+ *
+ * - Nodemon is a dev tool that will restart any node applications when file changes are detected in the directory.
+ */
+
+/**
+ * Express Routes
+ *
+ * When you are defining your Express Routes, you can use string patterns and regualr expressions.
+ *
+ * - 404 : Fail safe handler for missing endpoints.
+ * - 500 : Internal server error, will catch errors in code.
+ * - status : Additional Route. Status route to see status of application. Makes
+ *            users aware that API is working. Devs can monitor health of api as well.
+ * - signup : Allow users to sign up for the application.
+ * - login : Allow users to login to application. Might generate a user authentication
+ *           to allow other endpoint usage.
+ * - logout : Will revoke session + token.
+ * - token: Refresh a token.
+ * - forgot password : User forgets password
+ * - reset password : User reset password.
+ */
+
+/**
+ * Express Route Middleware
+ *
+ * - When you create a route(s) in your Express application, the order of the routes is important. When a request
+ *   comes in, Express will start trying to route that request to the different routes that you have defined started
+ *   top to bottom.
+ *
+ * - When you want to add an Error middleware handler, or a 404 handler, you need to make sure these are added at the
+ *   end after all your other routes.
+ */
--- Node.js and Express For Beginners/rest_api/index.js
+++ Node.js and Express For Beginners/rest_api/index.js
@@ -7,16 +7,71 @@ const port = 8080;
  * is the callback function we want to run at this request.
  */
 app.get('/', (request, response) => {
-  console.log(request);
   response.send('Hello World!');
 });
 
 /**
- * Example test end point that will send back "Test"
+ * Additional Route. Status route to see status of application. Makes
+ * users aware that API is working. Devs can monitor health of api as well.
  */
-app.get('/test', (request, response) => {
-  console.log(request);
-  response.send('test');
+app.get('/status', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Signup endpoint.
+ */
+app.post('/signup', (request, response, next) => {
+  next(new Error('test'));
+  // response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Login endpoint.
+ */
+app.post('/login', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Logout endpoint.
+ */
+app.post('/logout', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Token endpoint.
+ */
+app.post('/token', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Forget Password endpoint.
+ */
+app.post('/forgetpassword', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+/**
+ * Reset Password endpoint.
+ */
+app.post('/resetpassword', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+// Catch all other routes
+app.use((request, response) => {
+  response.status(404).json({ message: '404 - Not Found', status: 404 });
+});
+
+// Handling errors
+app.use((error, request, response, next) => {
+  console.log(error);
+  response
+    .status(error.status || 500)
+    .json({ error: error.message, status: 500 });
 });
 
 app.listen(port, () => {
